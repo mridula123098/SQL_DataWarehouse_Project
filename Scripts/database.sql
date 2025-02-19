27137056b08a234ce1b97164fb4d011fd9ae9ea0
Mridula Kandalgaonkar <136743574+mridula123098@users.noreply.github.com>
2025-02-19 18:46:01 +0530

    Create database.sql

---
+++ Scripts/database.sql
@@ -0,0 +1,16 @@
+USE master;
+GO
+  
+-- Create the 'DataWareHouse' database
+CREATE DATABASE IF NOT EXISTS DataWareHouse;
+GO
+  
+-- Create Schemas: 'Bronze','Silver','Gold'
+CREATE SCHEMA Bronze;
+GO
+  
+CREATE SCHEMA Silver;
+GO
+  
+CREATE SCHEMA Gold;
+GO
